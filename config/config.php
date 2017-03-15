72eac8884ff7c4b6282329dc762c87b03c928e06
kgd <kylegraydev@gmail.com>
2017-03-15 16:12:36 -0700

    changes form, adds responsiveness. text fixes

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
+
 return [
     'subject' => [
         'prefix' => '[Contact Form]'
     ],
     'emails' => [
-        'to'   => '',
-        'from' => ''
+        'to'   => 'wyldcard4pets@gmail.com',
+        'from' => 'no-reply@wyldcard.net'
     ],
     'messages' => [
         'error'   => 'There was an error sending, please try again later.',
@@ -15,10 +16,8 @@ return [
         'name'     => 'Name',
         'email'    => 'Email',
         'phone'    => 'Phone',
-        'subject'  => 'Subject',
+        // 'subject'  => 'Subject',
         'message'  => 'Message',
         'btn-send' => 'Send'
     ]
 ];
-
-?>

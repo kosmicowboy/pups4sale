377f73824b74a30593f0ffc0aca1952e2b14607b
kgd <kylegraydev@gmail.com>
2017-03-27 16:01:30 -0700

    test11

--- index.php
+++ index.php
@@ -1,3 +1,76 @@
+<?php
+
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Server = 'smtp.sendgrid.net'
+$mail->Port = '487'
+$username='apiapi'
+$password='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'
+
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+require 'vendor/autoload.php';
+$sendgrid = new SendGrid("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");
+$email    = new SendGrid\Email();
+
+$email->addTo("kylegraydev@gmail.com")
+      ->setFrom("wildcard4pets@me.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+
+
+// $EmailFrom = "test@gmail.com";
+// $EmailTo = "kylegraydev@gmail.com";
+// $Subject = "Nice & Simple Contact Form by CSS-Tricks";
+$Name = Trim(stripslashes($_POST['Name']));
+$Tel = Trim(stripslashes($_POST['Tel']));
+$Email = Trim(stripslashes($_POST['Email']));
+$Message = Trim(stripslashes($_POST['Message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+$sendgrid->send($email);
+
+// send email
+// $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+
+phpinfo();
+?>
+
+<?php echo '1'; ?>
+
+
+<html><p>Thank you for your email!</p></html>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -138,7 +211,7 @@
           <?php if (!empty($msg)) {
               echo "<h2>$msg</h2>";
           } ?>
-            <form method="post" action="./contactengine.php">
+            <form method="post" action="index.php">
               <label for="Name">Name:</label>
               <input type="text" name="Name" id="Name" />
 

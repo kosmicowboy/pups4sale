66b67100f3886ac42bdd3e51a9c7890414e6dab2
kgd <kylegraydev@gmail.com>
2017-03-28 01:34:56 -0700

    remove form

--- contactengine.php
+++ contactengine.php
@@ -1,72 +1,19 @@
 <?php
-
-$mail->IsSMTP();
-$mail->CharSet = 'UTF-8';
-$mail->Server = 'smtp.sendgrid.net'
-$mail->Port = '487'
-$username='apiapi'
-$password='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'
-
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-$sendgrid = new SendGrid("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");
-$email    = new SendGrid\Email();
-
-$email->addTo("kylegraydev@gmail.com")
-      ->setFrom("wildcard4pets@me.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
-
-
-
-// $EmailFrom = "test@gmail.com";
-// $EmailTo = "kylegraydev@gmail.com";
-// $Subject = "Nice & Simple Contact Form by CSS-Tricks";
-$Name = Trim(stripslashes($_POST['Name']));
-$Tel = Trim(stripslashes($_POST['Tel']));
-$Email = Trim(stripslashes($_POST['Email']));
-$Message = Trim(stripslashes($_POST['Message']));
-
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Tel: ";
-$Body .= $Tel;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-
-$sendgrid->send($email);
-
-// send email
-// $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-
-// redirect to success page
-if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
-}
-else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-}
-
-phpinfo();
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", "kylegraydev@gmail.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "kylegraydev@gmail.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 ?>
-
-<?php echo '1'; ?>
-
-
-<html><p>Thank you for your email!</p></html>
